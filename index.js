ad9cdd764af79ece8550d8dd0dd1ce9ee18fdb7d
Montanna Tilton <m.tilton@outlook.com>
2018-08-26 18:57:39 -0700

    debugging index.js errors now

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello world')
-})
+  res.send('hello world');
+});
