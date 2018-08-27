0165260acb5c0da69a6afc1e21ae3d4143bcc5fc
Montanna Tilton <m.tilton@outlook.com>
2018-08-26 19:00:06 -0700

    still debugging index.js bc idk how it works

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+});
+
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hello world');
