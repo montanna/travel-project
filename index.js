d7b5c1496c18fbe789a970e75959d96078c225ec
Montanna Tilton <m.tilton@outlook.com>
2018-08-26 20:25:40 -0700

    hello world! added comments + stylesheet

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000)); //this is necessary but I'm not sure why
 
 
-app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'))
+app.get('/', function (req, res) { //serve the index.html file from this directory 
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+app.listen(app.get('port'), function() { //this is necessary too
+    console.log("Node app is running at localhost:" + app.get('port'))
 });
